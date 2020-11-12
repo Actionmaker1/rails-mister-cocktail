7536d0fbb1c41f4b7dce8ee099506771abd7d490
Alexander von Croy <alexander@voncroy.eu>
2020-11-12 14:20:55 +0100

    rails-mister-cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+Ingredient.destroy_all
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: 'Mint')
+Ingredient.create(name: 'Vodka')
+Ingredient.create(name: 'Rhum')
+Ingredient.create(name: 'Tequila')
+Ingredient.create(name: 'Orange Juice')
+Ingredient.create(name: 'Tomato Juice')
